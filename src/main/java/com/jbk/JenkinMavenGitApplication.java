04029b7d7b276b1a0f5cafe5928b9b8e9b44d737
nileshDhaigude <nileshdhaigude@007gmail.com>
2023-04-10 02:35:01 -0800

    For war file

--- src/main/java/com/jbk/JenkinMavenGitApplication.java
+++ src/main/java/com/jbk/JenkinMavenGitApplication.java
@@ -2,12 +2,13 @@ package com.jbk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class JenkinMavenGitApplication {
+public class JenkinMavenGitApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinMavenGitApplication.class, args);
